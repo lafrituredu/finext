761d100587ce161987f63e3a8d6376eda42c2756
eru9927 <erubio.chaparro@gmail.com>
2026-04-05 08:19:51 +0200

    Nav route selected glow effect added.

--- Frontend/src/components/layout/Navbar.tsx
+++ Frontend/src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import DarkButton from '../buttons/DarkButton.tsx'
 import { useTranslation } from "react-i18next";
 import type { ChangeEvent } from "react";
@@ -6,11 +6,15 @@ import type { ChangeEvent } from "react";
 function Navbar() {
     const { i18n, t } = useTranslation("nav");
 
+    //Arrow function handleChangeLanguage: Detecta el "idioma" seleccionado
     const handleChangeLanguage = (e: ChangeEvent<HTMLSelectElement>) => {
         i18n.changeLanguage(e.target.value);
     };
 
     const currentLang = i18n.language.startsWith("en") ? "en" : "es";
+
+    //Arrow function linkClass: Detecta la ruta que esta activa y le da un estilo diferente
+    const linkClass = ({ isActive }: { isActive: boolean }) => `cursor-pointer px-10 rounded transition ${isActive ? "text-[#84A2EB] drop-shadow-[0_0_6px_rgb(59,130,246,0.2)] font-bold" : "text-black dark:text-[#D8E0F9] hover:text-[#9bb3ef]"}`
 
   return (
     <nav className="flex justify-between items-center fixed w-full bg-white dark:bg-[#040919] text-white h-20 dark:shadow-md px-10">
@@ -18,10 +22,10 @@ function Navbar() {
         <div className="flex flex-row inter">
             <img src="icons/finext.svg" alt="" className="w-10 h-10"/>
             <div className="flex justify-center items-center text-black dark:text-[#D8E0F9]">
-                <Link to="/home"><div className="pl-15 pr-10"><p className="cursor-pointer">{t("home")}</p></div></Link>
-                <Link to="/about"><div className="px-10"><p className="cursor-pointer">{t("about_us")}</p></div></Link>
-                <div className="px-10"><p className="cursor-pointer">{t("contact_us")}</p></div>
-                <div className="px-10"><p className="cursor-pointer">Something else</p></div>
+                <NavLink to="/home" className={linkClass}>{t("home")}</NavLink>
+                <NavLink to="/about" className={linkClass}>{t("about_us")}</NavLink>
+                <NavLink to="/contact" className={linkClass}>{t("contact_us")}</NavLink>
+                <NavLink to="/something" className={linkClass}>Something else</NavLink>
             </div>
 
         </div>
