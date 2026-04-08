7d9781aa7cac134274665ff0f0f1fad634da86ed
Marc Gilavert <marc.gilavert05@gmail.com>
2026-04-08 13:44:02 +0200

    correccion login

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -6,7 +6,7 @@ const Login: React.FC = () => {
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/register", {
+  const res = await fetch("http://127.0.0.1:8000/api/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
