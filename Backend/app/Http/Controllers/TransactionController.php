5236e93e92b2aabeaf4e64beff48a9abca766f89
erubio <erubio.chaparro@gmail.com>
2026-04-17 09:09:52 +0200

    Transacciones por user_id

--- Backend/app/Http/Controllers/TransactionController.php
+++ Backend/app/Http/Controllers/TransactionController.php
@@ -1,17 +1,26 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
     // GET TRANSACTIONS
-    public function index()
+    public function index(Request $request)
     {
-        $transactions = Transaction::with(['category' , 'user'])
-            ->orderBy('date', 'desc')
-            ->get();
+        $user = $request->user();
+        if($user->rol == 'autonomo'){
+            $transactions = Transaction::where('user_id', $user->id)
+                ->with(['category', 'user'])->get();
+        }else{
+            $transactions = Transaction::with(['category','user'])->get();
+        }
+        // $transactions = Transaction::with(['category' , 'user'])
+        //     ->orderBy('date', 'desc')
+        //     ->get();
 
         return response()->json($transactions);
     }
