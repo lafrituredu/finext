56dfb99ca664453c997400288b1056c1645e057c
erubio <erubio.chaparro@gmail.com>
2026-04-20 13:16:15 +0200

    Confirmation dark mode added

--- Frontend/src/components/materials/Confirmation.tsx
+++ Frontend/src/components/materials/Confirmation.tsx
@@ -17,13 +17,13 @@ export function Confirmation({Icon, children, close, onConfirm}:ConfirmationProp
   return (
     <>
         <div className="flex flex-col items-center justify-center fixed bg-[#0000006b] min-w-full min-h-full z-60 top-0 left-0">
-            <div className="inter flex flex-col items-center justify-center bg-white shadow-md rounded-xl p-8 m-2">
+            <div className="inter flex flex-col items-center justify-center bg-white dark:bg-dark-background dark:ring-2 dark:ring-dark-card shadow-md rounded-xl p-8 m-2">
                 {<Icon className="size-16 text-red-500 my-2"/>}
                 <p className="text-xl max-w-100 text-center">{children}</p>
                 <div className="flex flex-col justify-between pt-5 gap-4 w-full">
                     <button className="bg-red-400 rounded-full py-2 w-full cursor-pointer select-none hover:scale-104 hover:bg-red-500 
                     transition-all duration-100 ease-in-out" onClick={onConfirm}>Delete</button>
-                    <button className="bg-background ring-2 ring-gray-300 rounded-full py-2 w-full cursor-pointer select-none 
+                    <button className="bg-background ring-2 ring-gray-300 dark:bg-dark-background dark:ring-dark-card rounded-full py-2 w-full cursor-pointer select-none 
                     hover:scale-104 transition-all duration-100 ease-in-out" onClick={close}>Abort</button>
                 </div>
             </div>
