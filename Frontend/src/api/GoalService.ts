70beb5e8b0fe594539e8700897601aee1fcb20cb
jeremy_intriago <jeremy.secundaria@gmail.com>
2026-04-29 09:05:24 +0200

    getRecomendation

--- Frontend/src/api/GoalService.ts
+++ Frontend/src/api/GoalService.ts
@@ -21,3 +21,22 @@ export const contribute = async (id:number,cashToAdd:number) => {
     })
     return response.status
 } 
+export const getRecomendation = (goal:Goal,cashflow:number) => {
+    const percent = 0.35;
+    const difference = goal.target_amount - goal.current_amount;
+    
+    const now = new Date();
+    const end = new Date(goal.end_date);
+
+    const months = (end.getFullYear() - now.getFullYear()) * 12 + (end.getMonth() - now.getMonth());
+    
+    const goalMonthly = months != 0 ? difference / months : difference;
+
+    if (goalMonthly*1.15 > cashflow*percent) {
+        return -1;
+    }else if (goalMonthly < cashflow*percent*0.9){
+        return 1;
+    }else{
+        return 0;
+    }
+}
--- Frontend/src/pages/Goals.tsx
+++ Frontend/src/pages/Goals.tsx
@@ -1,17 +1,35 @@
 import React, { useEffect, useState } from 'react'
 
-import { getGoals, type Goal } from '../api/GoalService';
+import { getGoals, getRecomendation, type Goal } from '../api/GoalService';
 import TrashcanIcon from '/src/assets/icons/Trashcan.svg?react'
-
+import { deleteTransaction, getTransactions, type Transaction } from '../api/TransactionService'
 
 function Goals() {
   const [goals,setGoals] = useState<Goal[]>();
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<any>(null)
 
   useEffect(() => {
     getGoals()
     .then(value => setGoals(value))
     .catch(err => err)
+
+    getTransactions()
+      .then(data => setTransactions(data))
+      .catch(() => setError('Error al cargar las transacciones'))
+      .finally(() => setLoading(false));
   },[]);
+  
+  function calculateCashflow(){
+    const incomes = transactions
+        .filter(t => t.type === 'income' && new Date(t.date).getMonth() == new Date().getMonth())
+        .reduce((acc, t) => acc + Number(t.total_amount), 0)
+    const expenses = transactions
+        .filter(t => t.type === 'expense' && new Date(t.date).getMonth() == new Date().getMonth())
+        .reduce((acc, t) => acc + Number(t.total_amount), 0)
+    return (incomes - expenses).toFixed(2)
+  }
 
   return (
     <>
@@ -30,26 +48,9 @@ function Goals() {
             </button>
           </div>
 
-          {/* <div className='inter w-150 border border-[#0000001a] rounded-2xl px-8 py-5 flex flex-col gap-4 dark:bg-dark-card'>
-            <p className='font-semibold montserrat flex justify-between'>Goal Name <TrashcanIcon className='text-red-500' /> </p>
-            <div>
-              <p className='flex justify-between text-[#A1A1A1]'><span>140 días</span> <span>1400€</span></p>
-              <div className='relative w-full bg-[#D9D9D9] h-3 rounded-2xl overflow-hidden'>
-                <div className="bg-[#00540C] h-full" style={{ width: '90%' }} />
-              </div>
-            </div>
-
-            <div className='bg-[#98EE841a] p-2'>
-              <p>You’re <span className='text-green-600 font-semibold'>ahead of pace</span> and should reach your goal <b>30%</b> ahead of schedule</p>
-            </div>
-
-            <div className='w-full h-px bg-slate-200' />
-
-            <button className='w-full p-2 bg-black text-white dark:bg-primary cursor-pointer hover:scale-[102%] transition rounded-lg'>Aportar</button>
-          </div> */}
           <div className='w-full grid grid-cols-2 gap-15'>
             {goals?.map( (goal,key) => 
-            {
+            { const recomendation = getRecomendation(goal,parseInt(calculateCashflow()));
               const diffDays = Math.floor( (new Date(goal.end_date).getTime() - Date.now()) / (1000 * 60 * 60 * 24) )
               const progress = (goal.current_amount / goal.target_amount * 100).toFixed(2);
               return (
@@ -62,9 +63,10 @@ function Goals() {
                     </div>
                   </div>
 
-                  <div className='bg-[#98EE841a] p-2'>
-                    <p>You’re <span className='text-green-600 font-semibold'>ahead of pace</span> and should reach your goal <b>{progress}%</b> ahead of schedule</p>
-                  </div>
+                  {recomendation == 1 && <div className='bg-[#98EE841a] p-2'><p>You’re <span className='text-green-600 font-semibold'>ahead of pace</span> and should reach your goal <b>{progress}%</b> ahead of schedule</p></div>}
+                  {recomendation == -1 && <div className='bg-[#ee84841a] p-2'><p>You’re <span className='text-red-600 font-semibold'>ahead of pace</span> and should reach your goal <b>{progress}%</b> ahead of schedule</p></div>}
+                  {recomendation == 0 && <div className='bg-[#98EE841a] p-2'><p>You’re <span className='text-orange-600 font-semibold'>ahead of pace</span> and should reach your goal <b>{progress}%</b> ahead of schedule</p></div>}
+                    {/* <p>You’re <span className='text-green-600 font-semibold'>ahead of pace</span> and should reach your goal <b>{progress}%</b> ahead of schedule</p> */}
 
                   <div className='w-full h-px bg-slate-200' />
 
