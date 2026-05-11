5723d2b3eba97597a377b746bf2036cf2ca1c445
jeremy_secundaria <jeremy.secundaria@gmail.com>
2026-05-11 13:37:18 +0200

    Resumen mensual reports

--- Frontend/src/pages/ReportPage.tsx
+++ Frontend/src/pages/ReportPage.tsx
@@ -15,10 +15,10 @@ transactions.forEach( element => {
         return;
     }
     if (element.type == 'income') {
-        let total:number = filtered[date]?.incomes == undefined ? element.total_amount : Number(filtered[date].incomes) + Number(element.total_amount);
+        let total:number = filtered[date]?.incomes == undefined ? Number(element.total_amount) : Number(filtered[date].incomes) + Number(element.total_amount);
         filtered[date] = {incomes: total, expenses: filtered[date]?.expenses}
     }else{
-        let total:number = filtered[date]?.expenses == undefined ? element.total_amount : Number(filtered[date].expenses) + Number(element.total_amount);
+        let total:number = filtered[date]?.expenses == undefined ? Number(element.total_amount) : Number(filtered[date].expenses) + Number(element.total_amount);
         filtered[date] = {incomes: filtered[date]?.incomes , expenses: total}
     }
     
@@ -36,6 +36,8 @@ for (let i = 0; i <= (monthCounter-1); i++) {
     
     months.push(obj);
 }
+
+console.log(months)
 const getDynamicSeries = () => {
     switch (types) {
         case 'both':
@@ -75,10 +77,47 @@ const config = {
     series: getDynamicSeries(),
 };
   return (
-    <div id='report-content' className='p-10'>
-        <p className='flex justify-between montserrat text-sm mb-2'> <span>FULLNAME</span> <span>{new Date().toLocaleString()}</span> </p>
-        <div className='w-full h-px inter mb-10' />
-        <p className='text-xl'>Reporte de los {monthCounter} meses del año, {types == 'both' ? 'De ingresos y gastos' : `de solo ${types}`}</p>
+    <div id='report-content' className='py-20 px-25 relative flex flex-col gap-10'>
+        <div>
+            <p className='flex justify-between montserrat text-xl mb-2'> <span>FULLNAME</span> <span>{new Date().toLocaleString()}</span> </p>
+            <div className='w-full h-px bg-black inter mb-10' />
+        </div>
+
+        <div className='inter mb-5 flex flex-col gap-5'>
+            <p className='text-5xl mb-3'>Resumen general</p>
+            <ul>
+                <li className='text-3xl'>Total ingresos: 0000€</li>
+                <li className='text-3xl'>Total ingresos: 0000€</li>
+                <li className='text-3xl'>Total ingresos: 0000€</li>
+                <li className='text-3xl'>Total ingresos: 0000€</li>
+            </ul>
+        </div>
+
+        <div className='inter mb-5 flex flex-col gap-5'>
+            <p className='text-5xl mb-3'>Resumen mensual</p>
+            <table>
+                <thead>
+                    <tr className='*:text-start *:text-3xl'>
+                        <th>Mes - año</th>
+                        <th>Ingresos</th>
+                        <th>Gastos</th>
+                        <th>Fluoj</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {months.map( month => 
+                        <tr className='text-3xl'>
+                            <td>{month.name} {new Date().getFullYear()}</td>
+                            <td>{month.incomes}€</td>
+                            <td>{month.expense}€</td>
+                            <td>{month.incomes - month.expense}€</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+
+        <p className='text-5xl mb-3'>Grafica General</p>
         <Chart
         options={config.options}
         series={config.series}
@@ -86,6 +125,11 @@ const config = {
         width="100%"
         height={400}
         />
+        <div className='absolute bottom-10'>
+        <p>Este informe ha sido generado automáticamente por Finext.</p>
+        <p>Los datos fiscales son estimaciones orientativas. Consulta con un asesor profesional.</p>
+        </div>
+
     </div>
   )
   
