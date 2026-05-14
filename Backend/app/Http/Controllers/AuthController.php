b3dfb7928b00487ea173e44a43f45c5b58dd281c
Marc Gilavert <marc.gilavert05@gmail.com>
2026-05-14 13:13:20 +0200

    fix para server

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -112,7 +112,7 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => 'Login correcto',
-            'user' => $user,
+            'user' => $this->withAvatarUrl($user),
             'token' => $token
         ]);
     }
